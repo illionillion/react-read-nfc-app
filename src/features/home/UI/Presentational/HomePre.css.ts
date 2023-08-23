dc8e5482e51c4e53d5c1856c7d13061c6cb5b8cf
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-24 00:48:26 +0900

    feat: 未対応時のメッセージ表示

---
+++ src/features/home/UI/Presentational/HomePre.css.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+export const ContainerStyle = css`
+  height: calc(100vh - 8vh);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotSupportedMessage = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1vh;
+`;
--- src/features/home/UI/Presentational/HomePre.tsx
+++ src/features/home/UI/Presentational/HomePre.tsx
@@ -1,10 +1,29 @@
 import { FC } from "react";
 import { Layout } from "../../../../application/UI/Components/layout";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import { ContainerStyle, NotSupportedMessage } from "./HomePre.css";
 
 interface HomePreProps {
     isSupported: boolean
 }
 
-export const HomePre: FC<HomePreProps> = ({isSupported}) => {
-    return <><Layout>{isSupported ? "" : "お使いの端末はWebNFCはサポートされていません。"}</Layout></>
+export const HomePre: FC<HomePreProps> = ({ isSupported }) => {
+    return (
+        <>
+            <Layout>
+                {
+                    isSupported ?
+                        ""
+                        :
+                        <Container css={ContainerStyle}>
+                            <Box css={NotSupportedMessage}>
+                                <InfoOutlineIcon boxSize={7} />
+                                <Text>お使いの端末では対応しておりません。</Text>
+                            </Box>
+                        </Container>
+                }
+            </Layout>
+        </>
+    )
 }
