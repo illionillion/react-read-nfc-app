122586b573952ee1e30a4ae287a2220a3980a629
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-24 12:42:15 +0900

    add: eslint追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -8,11 +8,48 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react',  'unused-imports', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    // no-mixed-spaces-and-tabsルールをoffに設定する
+    'no-mixed-spaces-and-tabs': 'off',
+    // // インデントはスペースで統一
+    indent: ['error', 2, { SwitchCase: 1 }],
+
+    // // タブ文字は使用しない
+    // 'no-tabs': 'error',
+
+    /**
+     * @description importが型のみの場合は、import typeを強制
+     */
+    '@typescript-eslint/consistent-type-imports': 'error',
+
+    /**
+     * 未使用のimport削除
+     */
+    'unused-imports/no-unused-imports': 'warn',
+
+    /**
+     * @description propsを自動でソート
+     * @see {@link https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md}
+     */
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        ignoreCase: true,
+        noSortAlphabetically: true,
+        reservedFirst: true,
+      },
     ],
   },
 }
