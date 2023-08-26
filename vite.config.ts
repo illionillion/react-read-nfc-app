5f8ae257f3dd4c174b63274a66f28be8ad208e93
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-26 15:17:05 +0900

    fix: lint修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,40 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "react-read-nfc-app" : "./",
+  base: process.env.GITHUB_PAGES ? 'react-read-nfc-app' : './',
   plugins: [
     react(),
     basicSsl(),
     VitePWA({
-      includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
       manifest: {
-        short_name: "NFC Reader",
-        name: "NFC Reader",
+        short_name: 'NFC Reader',
+        name: 'NFC Reader',
         icons: [
           {
-            src: "favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
           },
           {
-            src: "logo192.png",
-            type: "image/png",
-            sizes: "192x192",
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
           },
           {
-            src: "logo512.png",
-            type: "image/png",
-            sizes: "512x512",
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
           },
         ],
-        start_url: ".",
-        display: "standalone",
-        theme_color: "#16630e",
-        background_color: "#ffffff",
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#16630e',
+        background_color: '#ffffff',
       },
     }),
   ],
