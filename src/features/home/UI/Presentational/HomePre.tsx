54c9b5dd96ac9737aecc5daf63d60662ca841fe8
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-26 11:39:48 +0900

    update:アイコンの変更

--- src/features/home/UI/Presentational/HomePre.tsx
+++ src/features/home/UI/Presentational/HomePre.tsx
@@ -4,26 +4,27 @@ import { NotSupportedMessage } from '../Components/NotSupportedMessage';
 import { Button, Container } from '@chakra-ui/react';
 import { ContainerStyle } from './HomePre.css';
 import { NFCReaderView } from '../Components/NFCReaderVIew';
+import { MdNfc } from 'react-icons/md';
 
 interface HomePreProps {
-    isSupported: boolean
-    isReadStarted: boolean
-    isRead: boolean
-    isReadFailed: boolean
-    errorMessage: string
-    NFCserialNumber: string
-    handleReadStart: MouseEventHandler<HTMLButtonElement>
-    records: readonly NDEFRecord[]
+  isSupported: boolean
+  isReadStarted: boolean
+  isRead: boolean
+  isReadFailed: boolean
+  errorMessage: string
+  NFCserialNumber: string
+  handleReadStart: MouseEventHandler<HTMLButtonElement>
+  records: readonly NDEFRecord[]
 }
 
-export const HomePre: FC<HomePreProps> = ({ isSupported, isRead, isReadStarted, isReadFailed, errorMessage, NFCserialNumber, handleReadStart, records}) => {
+export const HomePre: FC<HomePreProps> = ({ isSupported, isRead, isReadStarted, isReadFailed, errorMessage, NFCserialNumber, handleReadStart, records }) => {
   return (
     <>
       <Layout>
         <Container css={ContainerStyle}>
           {
             isSupported ?
-              !isReadStarted ? <><Button onClick={handleReadStart}>読み取り開始</Button></> : <NFCReaderView {...{ isRead, isReadFailed, errorMessage, NFCserialNumber, records }} />
+              !isReadStarted ? <><Button colorScheme='teal' rightIcon={<MdNfc />} onClick={handleReadStart}>読み取り開始</Button></> : <NFCReaderView {...{ isRead, isReadFailed, errorMessage, NFCserialNumber, records }} />
               :
               <NotSupportedMessage />
           }
