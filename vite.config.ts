624308a55b00634fc0b82b51aa27fd0bf401adfd
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-26 15:10:57 +0900

    update: pwa修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-    ? 'react-read-nfc-app'
-    : './',  
+  base: process.env.GITHUB_PAGES ? "react-read-nfc-app" : "./",
   plugins: [
     react(),
-    basicSsl()
+    basicSsl(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+      manifest: {
+        short_name: "NFC Reader",
+        name: "NFC Reader",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#16630e",
+        background_color: "#ffffff",
+      },
+    }),
   ],
 });
