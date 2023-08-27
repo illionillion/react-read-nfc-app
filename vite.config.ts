f1c9c24e69ba2b9f71869bcc5ee852459082d9ba
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-27 21:37:18 +0900

    fix: base修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'react-read-nfc-app' : './',
+  base: process.env.GITHUB_PAGES ? '/react-read-nfc-app/' : './',
   plugins: [
     react(),
     basicSsl(),
