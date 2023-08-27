138e46cb7f3aaeba4fbffaefdb151c449ea28485
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-27 13:16:10 +0000

    Update GitHub Pages

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-read-nfc-app/sw.js', { scope: '/react-read-nfc-app/' })})}
