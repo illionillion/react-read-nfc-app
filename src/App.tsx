4de136dc99538f46f5dbe616b7a485623e659263
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-27 21:25:24 +0900

    fix: 修正

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App: FC = () => {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<HomeCon />} />
           <Route path="read" element={<HomeCon />} />
