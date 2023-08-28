a93dfec63ec1f0004a6dcfaa6f3c5004b0ebee5a
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-28 12:46:49 +0900

    feat: 複数レコードの登録

--- src/features/write/UI/Container/WriteCon.tsx
+++ src/features/write/UI/Container/WriteCon.tsx
@@ -7,20 +7,28 @@ import { useBoolean } from '@chakra-ui/react';
 export const WriteCon: FC = () => {
   const [isSupported,] = useState<boolean>(isNFCSupported());
   const [data, setData] = useState<string>('');
+  const [writeData, setWriteData] = useState<NDEFRecordInit[]>([]);
   const [isWriting, setIsWriting] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
-  const [isWritingModal,{ on: WritingModalOpen, off: WritingModalClose}] = useBoolean();
-  const handleTextChange = (e:ChangeEvent<HTMLTextAreaElement>) => {
+  const [isWritingModal, { on: WritingModalOpen, off: WritingModalClose }] = useBoolean();
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setData(e.currentTarget.value);
   };
 
-  const handleToWrite = async() => {
+  const handleAddRecord = () => {
+    if (data === '') return;
+    setWriteData(prev => [...prev, { recordType: 'text', data: data }]);
+    setData('');
+  };
+
+  const handleToWrite = async () => {
+    if (writeData.length === 0) return;
     try {
       setIsWriting(true);
       setIsError(false);
       WritingModalOpen();
       const writer = new NDEFReader();
-      await writer.write(data);
+      await writer.write({ records: writeData });
     } catch (error) {
       console.log(error);
       setIsError(true);
@@ -29,5 +37,5 @@ export const WriteCon: FC = () => {
     }
   };
 
-  return <WritePre isError={isError} isSupported={isSupported} isWritingModal={isWritingModal}  data={data} isWriting={isWriting} handleTextChange={handleTextChange} handleToWrite={handleToWrite}WritingModalClose={WritingModalClose} />;
+  return <WritePre writeData={writeData} isError={isError} isSupported={isSupported} isWritingModal={isWritingModal} data={data} isWriting={isWriting} handleAddRecord={handleAddRecord} handleTextChange={handleTextChange} handleToWrite={handleToWrite} WritingModalClose={WritingModalClose} />;
 };
--- src/features/write/UI/Presentational/WritePre.tsx
+++ src/features/write/UI/Presentational/WritePre.tsx
@@ -1,6 +1,6 @@
 import type { ChangeEvent, FC } from 'react';
 import { Layout } from '../../../../application/UI/Components/layout';
-import { Box, Button, Container, Heading, Textarea } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Heading, List, ListItem, Text, Textarea } from '@chakra-ui/react';
 import { NotSupportedMessage } from '../../../../application/UI/Components/isSupported/NotSupportedMessage';
 import { ContainerStyle } from './WritePre.css';
 import { WritingModal } from '../Components/WritingModal';
@@ -11,11 +11,13 @@ interface WritePreProps {
   isWriting: boolean
   isWritingModal: boolean
   isError: boolean
+  writeData: NDEFRecordInit[]
   handleToWrite: () => Promise<void>
   handleTextChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
   WritingModalClose: () => void
+  handleAddRecord: () => void
 }
-export const WritePre: FC<WritePreProps> = ({ isSupported, data, isWriting, isWritingModal, isError, handleTextChange, handleToWrite, WritingModalClose }) => {
+export const WritePre: FC<WritePreProps> = ({writeData, isSupported, data, isWriting, isWritingModal, isError, handleTextChange, handleToWrite, WritingModalClose, handleAddRecord }) => {
   return (
     <Layout>
       <Container css={ContainerStyle}>
@@ -24,7 +26,19 @@ export const WritePre: FC<WritePreProps> = ({ isSupported, data, isWriting, isWr
             <Box>
               <Heading>テキストを入力</Heading>
               <Textarea value={data} onChange={handleTextChange} />
-              <Button onClick={handleToWrite}>書き込む</Button>
+              <Flex>
+                <Button onClick={handleAddRecord}>レコード追加</Button>
+                <Button onClick={handleToWrite}>書き込む</Button>
+              </Flex>
+              <Text>レコード一覧</Text>
+              <List>
+                {writeData.map((item, index) => (
+                  <ListItem key={index}>
+                    <Text>{item.data as string}</Text>
+                  </ListItem>
+                ))}
+                {writeData.length === 0 && <ListItem>レコードなし</ListItem>}
+              </List>
               <WritingModal isWriting={isWriting} isWritingModal={isWritingModal} isError={isError} WritingModalClose={WritingModalClose} />
             </Box>
             :
