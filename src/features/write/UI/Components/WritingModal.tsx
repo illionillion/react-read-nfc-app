7702c14a36689d39970e0b9fc66271a68c58bcb4
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-03 00:54:33 +0900

    update: 書き込み中ローディングにテキスト入れた
    contentでテキスト入れた

--- src/features/write/UI/Components/WritingModal.tsx
+++ src/features/write/UI/Components/WritingModal.tsx
@@ -15,8 +15,8 @@ export const WritingModal: FC<WritingModalProps> = ({ isWriting, isWritingModal,
       <ModalContent h='2xs'>
         <ModalBody h='full' display='flex' justifyContent='center' alignItems='center'>
           {isWriting ? <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' gap={3}>
-            <Text>書き込み中</Text>
-            <Spinner size='lg' />
+            {/* <Text>書き込み中</Text> */}
+            <Spinner size='lg' content='書き込み中' />
           </Box> : <Text>書き込み完了</Text>
           }
           {isError && <Text colorScheme='red'>書き込み失敗</Text>}
