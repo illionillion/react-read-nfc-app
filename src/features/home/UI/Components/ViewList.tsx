5c1ac9207a9697e3dc7c576b7f47f61424e0c75b
yuto ichinose <yuto.0526.sano@gmail.com>
2023-11-12 00:42:16 +0900

    refactor: #36 コンポーネントの外部化

---
+++ src/features/home/UI/Components/ViewList.tsx
@@ -0,0 +1,24 @@
+import { Link, Text } from "@chakra-ui/react";
+import { FC } from "react";
+
+interface ViewListProps {
+    item: NDEFRecord
+}
+
+export const ViewList: FC<ViewListProps> = ({ item }) => {
+    if (item.recordType === 'text') {
+        const textDecoder = new TextDecoder(item.encoding);
+        const text = textDecoder.decode(item.data);
+        return <Text>{`Text: ${text}`}</Text>;
+    } else if (item.recordType === 'url') {
+        const textDecoder = new TextDecoder();
+        const text = textDecoder.decode(item.data);
+        return <Text>URL: <Link isExternal overflowWrap='anywhere' href={text} color='teal.500'>{text}</Link></Text>;
+    } else if (item.mediaType === 'application/json' && item.recordType === 'mime') {
+        const textDecoder = new TextDecoder();
+        const text = textDecoder.decode(item.data);
+        return <Text>JSON: {text}</Text>;
+    } else {
+        return <Text>変換不可</Text>;
+    }
+}
