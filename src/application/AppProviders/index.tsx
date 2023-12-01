f7fc477981398b6c23b9cae19e6df6d0d1ee7dae
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-01 11:05:16 +0900

    refactor: プロバイダーを整理

--- src/application/AppProviders/index.tsx
+++ src/application/AppProviders/index.tsx
@@ -1,10 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import type { FC, ReactNode } from 'react';
+import type { FC } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { NotFoundCon } from '../../features/NotFound/UI/Container/NotFoundCon';
+import { HomeCon } from '../../features/home/UI/Container/HomeCon';
+import { WriteCon } from '../../features/write/UI/Container/WriteCon';
 
-type props = {
-    children: ReactNode
-}
-
-export const AppProviders: FC<props> = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+export const AppProviders: FC = () => {
+  return <ChakraProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path='/' element={<Navigate to='/read' />} />
+        <Route path='/read' element={<HomeCon />} />
+        <Route path='/write' element={<WriteCon />} />
+        <Route
+          path='*'
+          element={<NotFoundCon />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>;
 };
