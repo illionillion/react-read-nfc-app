bf0787c2c2574c5916d87bc969217ceba36267b3
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-21 10:58:21 +0900

    fix: ReactRouterDomのLinkを使うように修正

--- src/application/UI/Components/layout/HeaderItem/Container/HeaderItemCon.tsx
+++ src/application/UI/Components/layout/HeaderItem/Container/HeaderItemCon.tsx
@@ -4,14 +4,5 @@ import { HeaderItemPre } from '../Presentational/HeaderItemPre';
 
 export const HeaderItemCon: FC = () => {
   const pathname = useLocation().pathname;
-
-  const checkHref = (href:string) => {
-    if (import.meta.env.BASE_URL === '/react-read-nfc-app/') {
-      return `/react-read-nfc-app${href}`;
-    } else {
-      return href;
-    }
-  };
-
-  return <HeaderItemPre pathname={pathname} checkHref={checkHref}/>;
+  return <HeaderItemPre pathname={pathname}/>;
 };
--- src/application/UI/Components/layout/HeaderItem/Presentational/HeaderItemPre.tsx
+++ src/application/UI/Components/layout/HeaderItem/Presentational/HeaderItemPre.tsx
@@ -1,18 +1,18 @@
 import { Button, Flex, Heading, } from '@chakra-ui/react';
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { headerIconText, headerStyle } from './HeaderItemPre.css';
 
 interface HeaderItemPreProps {
     pathname: string
-    checkHref: (href: string) => string
 }
-export const HeaderItemPre: FC<HeaderItemPreProps> = ({pathname, checkHref}) => {
+export const HeaderItemPre: FC<HeaderItemPreProps> = ({pathname}) => {
   return (
     <Flex direction='column' as='header' css={headerStyle}>
-      <Heading css={headerIconText} as="a" href={checkHref('/')}>NFC読み取りWebアプリ</Heading>
+      <Heading css={headerIconText} as={Link} to="/">NFC読み取りWebアプリ</Heading>
       <Flex w='full' justify='center' gap={1}>
-        <Button flex={1} as='a' href={checkHref('/read')} colorScheme={pathname === '/' || pathname === '/read' ? 'teal' : undefined }>読み取りモード</Button>
-        <Button flex={1} as='a' href={checkHref('/write')} colorScheme={pathname === '/write' ? 'teal' : undefined }>書き込みモード</Button>
+        <Button flex={1} as={Link} to="/read" colorScheme={pathname === '/' || pathname === '/read' ? 'teal' : undefined }>読み取りモード</Button>
+        <Button flex={1} as={Link} to="/write" colorScheme={pathname === '/write' ? 'teal' : undefined }>書き込みモード</Button>
       </Flex>
     </Flex>
   );
