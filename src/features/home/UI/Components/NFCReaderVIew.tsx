5add0cc7e722fe7ae42c2ee093f5cf02e460bdd0
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-04 16:39:31 +0900

    feat: アニメーション実装

--- src/features/home/UI/Components/NFCReaderVIew.tsx
+++ src/features/home/UI/Components/NFCReaderVIew.tsx
@@ -1,16 +1,16 @@
 import { Box, Text } from '@chakra-ui/react';
 import type { FC } from 'react';
-import { IconContext } from 'react-icons';
-import { LuSmartphoneNfc } from 'react-icons/lu';
 import { IsSupportedMessageStyle } from '../../../../application/UI/Components/isSupported/NotSupportedMessage.css';
 import { ViewList } from './ViewList';
+import { Player } from '@lottiefiles/react-lottie-player';
+import NFCAnimation from '../../../../../public/lottie/nfc-animation.json'
 
 interface NFCReaderViewProps {
-    isRead: boolean
-    isReadFailed: boolean
-    errorMessage: string
-    NFCserialNumber: string
-    records: readonly NDEFRecord[]
+  isRead: boolean
+  isReadFailed: boolean
+  errorMessage: string
+  NFCserialNumber: string
+  records: readonly NDEFRecord[]
 }
 
 export const NFCReaderView: FC<NFCReaderViewProps> = ({ isRead, isReadFailed, errorMessage, NFCserialNumber, records }) => {
@@ -18,7 +18,7 @@ export const NFCReaderView: FC<NFCReaderViewProps> = ({ isRead, isReadFailed, er
     {
       isRead ?
         <>
-          {isReadFailed ? errorMessage : 
+          {isReadFailed ? errorMessage :
             <Box h='full'>
               <Text py={5}>シリアルナンバー：{NFCserialNumber}</Text>
               {records.map((item, index) => <ViewList key={index} item={item} />)}
@@ -26,9 +26,16 @@ export const NFCReaderView: FC<NFCReaderViewProps> = ({ isRead, isReadFailed, er
           }
         </> :
         <Box css={IsSupportedMessageStyle}>
-          <IconContext.Provider value={{ size: '3rem' }}>
-            <LuSmartphoneNfc />
-          </IconContext.Provider>
+          <Player
+            autoplay
+            loop
+            src={NFCAnimation}
+            style={{
+              height: "250px",
+              width: "250px",
+              pointerEvents: "none",
+            }}
+          />
           <Text>NFCを近づけてください。</Text>
         </Box>
     }
