e2f4ffca1bdde0f0d26818547673ffae64b78fed
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-05 20:42:02 +0900

    feat: 未対応端末時のアイコンも追加

--- src/application/UI/Components/isSupported/NotSupportedMessage.tsx
+++ src/application/UI/Components/isSupported/NotSupportedMessage.tsx
@@ -3,12 +3,21 @@ import type { FC } from 'react';
 import { IconContext } from 'react-icons';
 import { MdSystemSecurityUpdateWarning } from 'react-icons/md';
 import { IsSupportedMessageStyle } from './NotSupportedMessage.css';
+import NotSupportedIcon from '../../../../../public/lottie/not-supported.json'
+import { Player } from '@lottiefiles/react-lottie-player';
 
 export const NotSupportedMessage: FC = () => {
   return <Box css={IsSupportedMessageStyle}>
-    <IconContext.Provider value={{ size: '3rem' }}>
-      <MdSystemSecurityUpdateWarning />
-    </IconContext.Provider>
+    <Player
+      autoplay
+      loop
+      src={NotSupportedIcon}
+      style={{
+        height: "250px",
+        width: "250px",
+        pointerEvents: "none",
+      }}
+    />
     <Text>お使いの端末では対応しておりません。</Text>
   </Box>;
 
