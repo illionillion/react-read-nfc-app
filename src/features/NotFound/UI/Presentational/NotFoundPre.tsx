a39402ec273c6386899e24771a081e18f7fafda9
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-06 08:09:22 +0900

    feat: NotFoundのアイコン追加

--- src/features/NotFound/UI/Presentational/NotFoundPre.tsx
+++ src/features/NotFound/UI/Presentational/NotFoundPre.tsx
@@ -1,6 +1,8 @@
 import { Container, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { Player } from '@lottiefiles/react-lottie-player';
 import type { FC } from 'react';
 import { Link } from 'react-router-dom';
+import NotFoundAnimation from '../../../../../public/lottie/not-found.json';
 import { Layout } from '../../../../application/UI/Components/layout';
 import { NotFoundContainer } from './NotFound.css';
 
@@ -8,6 +10,16 @@ export const NotFoundPre: FC = () => {
   return (
     <Layout>
       <Container css={NotFoundContainer}>
+        <Player
+          autoplay
+          loop
+          src={NotFoundAnimation}
+          style={{
+            height: '250px',
+            width: '250px',
+            pointerEvents: 'none',
+          }}
+        />
         <Text>ページが見つかりません。</Text>
         <ChakraLink as={Link} to='/' color="skyblue" textDecoration="underline">トップへ戻る</ChakraLink>
       </Container>
